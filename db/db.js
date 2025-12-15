2b816db7368bf42cf07bd0aebca78a704cf2c974
Lina <linabenmouh0@gmail.com>
2025-12-15 20:10:57 +0100

    Render

--- db/db.js
+++ db/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
-  password: process.env.PGPASSWORD
+  password: process.env.PGPASSWORD,
+  ssl: isProd ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post("/register", (req, res) => {
+/*router.post("/register", (req, res) => {
   console.log("✅ /api/auth/register HIT", req.body);
   return res.status(200).json({ ok: true, body: req.body });
 });
+*/
 
-
-//router.post('/register', authController.register);
+router.post('/register', authController.register);
 router.post('/login', authController.login);
 
 module.exports = router;
